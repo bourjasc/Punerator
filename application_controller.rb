c5d5838d91aefc316bce2c9b41bfc143caf0e0a3
aasthana1400 <ayaan.asthana1400@gmail.com>
2015-07-02 18:42:31 +0000

    worked on buttons and the connections. not sure if finished.

--- application_controller.rb
+++ application_controller.rb
@@ -1,11 +1,14 @@
 require 'bundler'
 Bundler.require
 require_relative 'models/punerator.rb'
-
 class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
   end
+  post '/category' do 
+    pun = Pun.new
+    @result = Pun.choose_category(params[:category])
+    erb :result
 
 end
