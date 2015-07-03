a037fa15695772162390f77e5b4cf09852ab02d6
bourjasc <bourjasc@gmail.com>
2015-07-03 15:27:31 +0000

    final copy pt 2

--- application_controller.rb
+++ application_controller.rb
@@ -1,6 +1,7 @@
 require 'bundler'
 Bundler.require
 require_relative 'models/punerator.rb'
+require_relative 'models/twilio.rb'
 class ApplicationController < Sinatra::Base
 
   get '/' do
@@ -11,5 +12,11 @@ class ApplicationController < Sinatra::Base
     @result = pun.choose_category(params[:category])
     erb :result
   end
+  post '/text' do
+    twilio(params[:number], params[:message])
+    @message = "Your pun has been pun-ted"
+    erb :index
+  end
+  
 
 end
